2378313fe5405ff8a0a14ba41dc99b9f1b867f91
Kyrellos Saad Ibrahim <kyrellosibrahim@gmail.com>
2026-02-26 15:23:15 -0600

    icon behaviour

--- Assets/Scripts/IconBehaviour.cs
+++ Assets/Scripts/IconBehaviour.cs
@@ -2,6 +2,7 @@ using UnityEngine;
 
 public class IconBehaviour : MonoBehaviour
 {
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -11,6 +12,16 @@ public class IconBehaviour : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+
+        // add delay to the rotation so they don't all rotate in sync
+        float delay = Random.Range(0f, 2f);
+        if (Time.time % 2f < delay)
+        {
+            transform.Rotate(0, 0, 45 * Time.deltaTime);
+        }
+        else
+        {
+            transform.Rotate(0, 0, -45 * Time.deltaTime);
+        }
     }
 }
